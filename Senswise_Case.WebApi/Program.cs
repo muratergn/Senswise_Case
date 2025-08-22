6370a69d5f00f89796acce46fbb7cc2241f0e70f
murat <murat@Victus>
2025-08-22 19:23:21 +0300

    Final

--- Senswise_Case.WebApi/Program.cs
+++ Senswise_Case.WebApi/Program.cs
@@ -1,4 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Senswise_Case.Application.Interfaces;
+using Senswise_Case.Persistence.Repository;
+using Senswise_Case.Application.Features.CQRS.Handlers;
+using Senswise_Case.Application.Interfaces;
+using Senswise_Case.Persistence.Context;
+using Senswise_Case.Persistence.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+builder.Services.AddScoped<CreateUserCommandHandler>();
+builder.Services.AddScoped<GetUserByIdQueryHandler>();
+builder.Services.AddScoped<GetUserQueryHandler>();
+builder.Services.AddScoped<UpdateUserCommandHandler>();
+builder.Services.AddScoped<RemoveUserCommandHandler>();
+
 
 // Add services to the container.
 
@@ -6,6 +23,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
 
 var app = builder.Build();
 
